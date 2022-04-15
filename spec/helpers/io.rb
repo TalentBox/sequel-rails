07bf7d012427b4a051052a82d2ca566bb3a84b05
Jonathan Tron <jonathan@openhood.com>
2022-04-15 11:10:30 +0200

    Fix specs for Rails >= 6.1

--- spec/helpers/io.rb
+++ spec/helpers/io.rb
@@ -4,5 +4,10 @@ module IOSpecHelper
       # if this isn't a good use for case equality I don't know what is
       pattern === a.first ? raise(Errno::ENOENT) : m.call(*a) # rubocop:disable CaseEquality
     end
+    # Rails > 6.1 uses Pathname#exists? instead of rescuing read error
+    allow(Pathname).to receive(:new).and_call_original
+    allow(Pathname).to receive(:new).with(pattern).and_return(
+      instance_double(Pathname, :exist? => false)
+    )
   end
 end
