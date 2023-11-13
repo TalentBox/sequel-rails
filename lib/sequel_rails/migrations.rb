d775de6ce91653349e8e177dcd9bbba2acf6cd8b
PikachuEXE <pikachuexe@gmail.com>
2023-11-13 15:50:05 +0800

    * Update db:migrate:up & db:migrate:down to act more like active record one

--- lib/sequel_rails/migrations.rb
+++ lib/sequel_rails/migrations.rb
@@ -3,13 +3,12 @@ require 'sequel/extensions/migration'
 module SequelRails
   class Migrations
     class << self
-      def migrate(version = nil)
-        opts = {}
+      def migrate(version = nil, migrator_class = ::Sequel::Migrator, opts = {})
         opts[:target] = version.to_i if version
         opts[:allow_missing_migration_files] = !!SequelRails.configuration.allow_missing_migration_files
 
         if migrations_dir.directory?
-          ::Sequel::Migrator.run(::Sequel::Model.db, migrations_dir, opts)
+          migrator_class.run(::Sequel::Model.db, migrations_dir, opts)
         else
           relative_path_name = migrations_dir.relative_path_from(Rails.root).to_s
           raise "The #{relative_path_name} directory doesn't exist, you need to create it."
@@ -17,6 +16,14 @@ module SequelRails
       end
       alias_method :migrate_up!, :migrate
       alias_method :migrate_down!, :migrate
+
+      def migrate_up_one_version!(version)
+        migrate(version, SequelRails::OneTimestampVersionMigrator, { direction: :up })
+      end
+
+      def migrate_down_one_version!(version)
+        migrate(version, SequelRails::OneTimestampVersionMigrator, { direction: :down })
+      end
 
       def pending_migrations?
         return false unless available_migrations?
@@ -76,6 +83,49 @@ module SequelRails
           allow_missing_migration_files: !!SequelRails.configuration.allow_missing_migration_files
         )
       end
+    end
+  end
+
+  class OneTimestampVersionMigrator < ::Sequel::TimestampMigrator
+    Error = ::Sequel::Migrator::Error
+
+    # The direction of the migrator, either :up or :down
+    attr_reader :direction
+
+    # Set up all state for the migrator instance
+    def initialize(db, directory, opts = OPTS)
+      @direction = opts[:direction]
+      super
+
+      raise(Error, 'target (version) is absent') if target.nil?
+      raise(Error, "Invalid direction: #{direction}") unless [:up, :down].include?(direction)
+      # `allow_missing_migration_files` ignored, as version is from user input
+      raise(Error, "Migration with version #{target} not found") if migration_tuples.empty?
+    end
+
+    private
+
+    def get_migration_tuples
+      up_mts = []
+      down_mts = []
+      files.each do |path|
+        f = File.basename(path)
+        fi = f.downcase
+        next unless migration_version_from_file(f) == target
+
+        case [direction, applied_migrations.include?(fi)]
+        when [:up, false]
+          # Normal migrate up
+          up_mts << [load_migration_file(path), f, :up]
+        when [:down, true]
+          # Normal migrate down
+          down_mts << [load_migration_file(path), f, :down]
+        else
+          # Already run, don't re-run
+          raise(Error, "Migration with version #{target} is already migrated/rollback")
+        end
+      end
+      up_mts + down_mts.reverse
     end
   end
 end
